6f1fb0424c70a2bfc02cb615b8f9869861a9de74
Luiza Deolindo <luizacristina2@gmail.com>
2022-01-15 17:39:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = document.querySelector("#msg")
+    var img = document.querySelector("#imagem")
+     var agora = new Date()
+     var hora = agora.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora < 12) {
+        img.src = "manha.png"
+        document.body.style.background = "#b8c9e7"
+    } else if(hora >= 12 && hora <= 18) {
+        img.src = "tarde.png"
+        document.body.style.background = "#d86e1d"
+    } else {
+        img.src = "noite.png"
+        document.body.style.background = "#263345"
+    }
+}
